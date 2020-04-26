ffced9673587e4068ce904db5c365b8dd797a69c
guptabless <61534368+guptabless@users.noreply.github.com>
2020-04-26 14:33:49 -0400

    Create dnsinspector.py

---
+++ dnsinspector.py
@@ -0,0 +1,62 @@
+import dns.resolver
+import bcolors
+
+print("-----------------------------------------------")
+print("Code By : NG")
+print("Usage: python dnsinspector.py and then follow the instructions")
+print("Give URl without http:// or https://")
+print("-----------------------------------------------")
+
+url = str(input(bcolors.BLUE + "Enter URL For which you want to check DNS record"))
+
+print(bcolors.BLUE + '********CNAME Record***********')
+try:
+    cname_user = dns.resolver.query(url, 'CNAME')
+    for data in cname_user:
+        print(bcolors.PASS)
+        print(data)
+except :
+    print(bcolors.ERRMSG + "CNAME Not found"+ '\n')
+
+#MX record
+print(bcolors.BLUE + '************MX Record***********')
+try:
+    mx_user = dns.resolver.query(url, 'MX')
+
+    for mxdata in mx_user:
+        print(bcolors.PASS)
+        print(mxdata)
+except :
+    print(bcolors.ERRMSG + "MX Record Not found"+ '\n')
+
+#A record
+print('\n'+ bcolors.BLUE +  '************A Record********')
+try:
+    a_user = dns.resolver.query(url, 'A')
+    for adata in a_user:
+        print(bcolors.PASS)
+        print(adata)
+except :
+    print(bcolors.ERRMSG + "A Record Not found"+ '\n')
+
+# NS record
+print('\n'+ bcolors.BLUE + '**********Name Server Record*******')
+try:
+    ns_user = dns.resolver.query(url, 'NS')
+    for nsdata in ns_user:
+        print(bcolors.PASS)
+        print(nsdata)
+except:
+    print(bcolors.ERRMSG + "NameServer Record Not found"+ '\n')
+
+print('\n'+ bcolors.BLUE + '*******SPF Record**********')
+try:
+    spf_user = dns.resolver.query(url, 'SPF')
+    for spfdata in spf_user:
+        print(bcolors.PASS)
+        print(spfdata)
+except:
+    print(bcolors.ERRMSG + "SPF Record Not found"+ '\n')
+
+
+
